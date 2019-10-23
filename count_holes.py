13df42f1f291fbd555ca0fa7ddeec8ec66bb3a3c
ooliynyk <ooliynyk@llnw.com>
2019-10-23 14:53:19 +0300

    count frequent

--- count_holes.py
+++ count_holes.py
@@ -1,44 +1,11 @@
 def count_holes(n):
     counter = 0
-    if type(n) is float:
-        print ("ERROR")
-    elif type(n) is int:
-        modified = list(str(n))
-        print(modified)
-        while modified[0] == "0":
-            modified.remove('0')
-        else:
-            one = ['4', '6', '9', '0']
-            for i in modified:
-                if i == '0':
-                    pass
-                if i in one:
+    norm = str(n).lstrip('-0 ')
+    if not norm.isdigit():
+        return "ERROR"
+    for i in norm:
+                if i in '4690':
                     counter += 1
                 elif i == '8':
                     counter += 2
-                else:
-                    pass
-            return counter
-    elif type(n) is str:
-        modified = list(n)
-        print(modified)
-        if modified[0] == "'":
-            modified.remove("'")
-        else:
-            pass
-        while modified[0] == "0":
-            modified.remove('0')
-        else:
-            one = ['4', '6', '9', '0']
-            for i in modified:
-                if i == '0':
-                    pass
-                if i in one:
-                    counter += 1
-                elif i == '8':
-                    counter += 2
-                else:
-                    pass
-            return counter
-    else:
-        print(0)
+    return counter
--- find_most_frequent.py
+++ find_most_frequent.py
@@ -2,6 +2,7 @@ import collections
 from collections import Counter
 import string
 
+#with imported counter
 def find_most_frequent(text):
     text = "".join(w for w in text if w not in string.punctuation)
     text = text.lower().split()
@@ -9,10 +10,21 @@ def find_most_frequent(text):
     value_list = count.values()
     max_num = max(value_list)
     fr = []
-    for key, value in count.items():
-        if value == max_num:
-            fr.append(key)
-        else:
-            pass
-    fr.sort()
+    fr = fr.append(key for key, value in count if value == max_num)
     print(fr)
+    fr.sort()
+    return fr
+    
+    
+#most_frequent2, with own counter
+def find_most_frequent2(text):
+    text = "".join(w for w in text if w not in string.punctuation)
+    text = text.lower().split()
+    counter = {}
+    for word in text:
+        counter[word] = counter.get(word, 0) + 1
+    print(counter)
+    max_value = max(counter.values())
+    most_fr = [key for key, value in counter.items() if value == max_value]
+    most_fr.sort()
+    print(most_fr)
