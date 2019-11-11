b2b8608bb5bb32ca52d09523face160b2fac9d15
ooliynyk <ooliynyk@llnw.com>
2019-11-11 08:20:27 +0200

    typing errors

--- bubble_sorting_method.py
+++ bubble_sorting_method.py
@@ -1,13 +1,13 @@
 def bubbles(n, n_rev):
-    # define the number of compearings in n. This allows to escape the error of comparings of last element with no element. 
-    numberOfComp = len(n) -1
-    # Each element should be compeared
-    for i in range(numberOfComp):
+    # define the number of comparings in n. This allows to escape the error of comparings of last element with no element. 
+    number_of_comp = len(n) -1
+    # Each element should be compared
+    for i in range(number_of_comp):
         # take element j from the n and compare to element j+1
-        for j in range(numberOfComp - i):
+        for j in range(number_of_comp - i):
             if n[j] > n[j+1]:
                 # if first element is greater, it becomes the second element
                 n[j], n[j+1] = n[j+1], n[j]
     #reversed sorting
     n_rev = n[::-1]
-    return n, n_ver
+    return n, n_rev
